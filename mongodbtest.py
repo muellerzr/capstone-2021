004666ed22ef5fd81898bf4d4f12fa2223692c4f
Nick Norris <nickjamesnorris@gmail.com>
2021-10-28 16:14:57 -0500

    Mongo instance is connected

---
+++ mongodbtest.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient('mongodb+srv://Nick:BhKkB9LmdRMfA@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db=client.test
+# Issue the serverStatus command and print the results
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
