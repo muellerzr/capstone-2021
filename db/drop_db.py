ae505be854116193f7071e5bec4481682bccf813
Nick Norris <nickjamesnorris@gmail.com>
2021-11-17 21:13:04 -0600

    clean up

--- db/drop_db.py
+++ db/drop_db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 # client = MongoClient('mongodb+srv://Nick:BhKkB9LmdRMfA@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
-client = MongoClient('mongodb+srv://Gabe:Pa55w0rd!@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://<username>:<password>@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
 # db = client.business
 db = client.credentials
 
