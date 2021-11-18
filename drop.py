8e117d2bf76bc3186cdf11c897a9c32d7140b2df
gc65 <gc65@students.uwf.edu>
2021-11-17 18:27:15 -0600

    commented out nick\'s connection string, added gabe\'s connection string

--- drop.py
+++ drop.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://Nick:BhKkB9LmdRMfA@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db=client.business
+# client = MongoClient('mongodb+srv://Nick:BhKkB9LmdRMfA@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://Gabe:Pa55w0rd!@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
+# db = client.business
+db = client.credentials
 
-db.reviews.drop()
+db.credentials.drop()
