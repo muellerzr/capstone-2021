e7477a519a5332728dd6aaaf719d365021f1b9e4
gc65 <gc65@students.uwf.edu>
2021-11-17 18:26:40 -0600

    commented nick\'s connection string, added gabe\'s connection string

--- connection_test.py
+++ connection_test.py
@@ -4,7 +4,7 @@ from random import randint
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 # client = MongoClient('mongodb+srv://Nick:BhKkB9LmdRMfA@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
 client = MongoClient('mongodb+srv://Gabe:Pa55w0rd!@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db=client.credentials
+db = client.credentials
 
 serverStatusResult=db.command("serverStatus")
 print(serverStatusResult)
