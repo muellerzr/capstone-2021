df91ac24dde01a6a506ca4093a7d8deac793d07e
gc65 <gc65@students.uwf.edu>
2021-11-22 18:19:50 -0600

    parameterized connection string using f-string templates

--- db/connection_test.py
+++ db/connection_test.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
-# pprint library is used to make the output look more pretty
 from random import randint
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-# client = MongoClient('mongodb+srv://Nick:BhKkB9LmdRMfA@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
-client = MongoClient('mongodb+srv://Gabe:Pa55w0rd!@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority')
+username = "Gabe"
+password = "Pa55w0rd!"
+url = f'mongodb+srv://{username}:{password}@cluster0.27gwi.mongodb.net/Cluster0?retryWrites=true&w=majority'
+client = MongoClient(url)
 db = client.credentials
 
 serverStatusResult=db.command("serverStatus")
